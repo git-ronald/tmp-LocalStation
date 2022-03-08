dbfa0a155672f2b8426ed2d8d3ac235722add873
git-ronald <inbox.ronald@outlook.com>
2022-03-09 00:34:46 +0100

    Public access to Schedule

--- TestAppLibrary/Scheduler/FixedTimeSchedulerConfig.cs
+++ TestAppLibrary/Scheduler/FixedTimeSchedulerConfig.cs
@@ -6,47 +6,27 @@ namespace TestAppLibrary.Scheduler
 {
     internal class FixedTimeSchedulerConfig : PeerFixedTimeConfig
     {
-        public override Dictionary<TimeSpan, SchedulerTaskList> BuildSchedule<TState>(TState? state = default) where TState : class
+        public override Dictionary<TimeSpan, SchedulerTaskList> BuildSchedule(SchedulerState state)
         {
             var schedule = base.BuildSchedule(state);
 
-            var existingEvent = schedule.First();
-            existingEvent.Value.Add(
-                token =>
-                {
-                    Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss} Fixed time 1 by TestApp {existingEvent.Key}");
-                    return Task.CompletedTask;
-                });
+            //var existingEvent = schedule.First();
+            //existingEvent.Value.Add(
+            //    token =>
+            //    {
+            //        Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss} Fixed time 1 by TestApp {existingEvent.Key}");
+            //        return Task.CompletedTask;
+            //    });
 
-            var nextEventTime = DateTime.UtcNow.Apply(existingEvent.Key, 0).AddMinutes(1).TimeOfDay;
-            schedule.Ensure(nextEventTime).Add(
-                token =>
-                {
-                    Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss} Fixed time 2 by TestApp {nextEventTime}");
-                    return Task.CompletedTask;
-                });
+            //var nextEventTime = DateTime.UtcNow.Apply(existingEvent.Key, 0).AddMinutes(1).TimeOfDay;
+            //schedule.Ensure(nextEventTime).Add(
+            //    token =>
+            //    {
+            //        Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss} Fixed time 2 by TestApp {nextEventTime}");
+            //        return Task.CompletedTask;
+            //    });
 
             return schedule;
         }
-        //public override Dictionary<TimeSpan, SchedulerTaskList> BuildSchedule<TState>(TState? state = default(TState)) where TState : notnull
-        //{
-        //    var schedule = base.BuildSchedule<TState>(state);
-        //    return schedule;
-        //}
-        //public FixedTimeSchedulerConfig()
-        //{
-        //    Tasks.Ensure(new TimeSpan(21, 14, 0)).AddItems(
-        //        (token, state) =>
-        //        {
-        //            Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss} Fixed time 1 by TestApp");
-        //            return Task.CompletedTask;
-        //        });
-        //    Tasks.Ensure(new TimeSpan(21, 15, 0)).AddItems(
-        //        (token, state) =>
-        //        {
-        //            Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss} Fixed time 2 by TestApp");
-        //            return Task.CompletedTask;
-        //        });
-        //}
     }
 }
--- TestAppLibrary/Scheduler/TimeCompartmentSchedulerConfig.cs
+++ TestAppLibrary/Scheduler/TimeCompartmentSchedulerConfig.cs
@@ -5,38 +5,17 @@ using PeerLibrary.Scheduler;
 
 namespace TestAppLibrary.Scheduler
 {
-    internal class TimeCompartmentSchedulerConfig : PeerCompartmentConfig //ISchedulerConfig<object, TimeCompartments>
+    internal class TimeCompartmentSchedulerConfig : PeerCompartmentConfig
     {
-        //public TimeCompartmentSchedulerConfig()
-        //{
-        //    //Tasks[TimeCompartments.Every2Minutes] = new()
-        //    //{
-        //    //    ScheduleEvery2Minutes1,
-        //    //    ScheduleEvery2Minutes2,
-        //    //};
-
-        //    //Tasks[TimeCompartments.Every3Minutes] = new()
-        //    //{
-        //    //    ScheduleEvery3Minutes
-        //    //};
-
-        //    Tasks.Ensure(TimeCompartments.Every2Minutes).AddItems(
-        //        ScheduleEvery2Minutes1,
-        //        ScheduleEvery2Minutes2);
-
-        //    Tasks.Ensure(TimeCompartments.Every3Minutes).AddItems(
-        //        ScheduleEvery3Minutes);
-        //}
-
-        public override Dictionary<TimeCompartments, SchedulerTaskList> BuildSchedule<TState>(TState? state = null) where TState : class
+        public override Dictionary<TimeCompartments, SchedulerTaskList> BuildSchedule(SchedulerState state)
         {
-            var schedule = base.BuildSchedule<TState>(state);
-            schedule.Ensure(TimeCompartments.EveryMinute).Add(
-                token =>
-                {
-                    Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss} Every minute by TestApp");
-                    return Task.CompletedTask;
-                });
+            var schedule = base.BuildSchedule(state);
+            //schedule.Ensure(TimeCompartments.EveryMinute).Add(
+            //    token =>
+            //    {
+            //        Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss} Every minute by TestApp");
+            //        return Task.CompletedTask;
+            //    });
 
             return schedule;
         }
